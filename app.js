e9eb187455c4e6f412c04d289e222fa8107ca953
Marina Girolimetto <marina.gtto@yahoo.com.br>
2020-08-10 14:05:04 -0300

    Create app.js
    Adicionando primeiros códigos NodeJs.

---
+++ app.js
@@ -0,0 +1,18 @@
+// Imprimindo no console
+console.log('Ola Mundo')
+
+// Imprimindo nome e sobrenome no console
+var nome = "Marina"
+var sobrenome = "Costa"
+
+console.log(nome + " " + sobrenome)
+
+// Criando função para retornar soma de dois números
+var num1 = 10
+var num2 = 20
+
+function somar(a, b) {
+    return a + b
+}
+
+console.log(somar(num1, num2))
