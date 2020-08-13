ddeffcd53bfe652d1670bf178d3dc05d9241f4b4
Marina Girolimetto <marina.gtto@yahoo.com.br>
2020-08-13 10:53:04 -0300

    Ajustes

--- Express/http_express.js
+++ Express/http_express.js
@@ -18,7 +18,17 @@ app.get("/blog", function(req, res){
     res.send("Bem vindo ao meu BLOG") // envia mensagem ao usuário
 });
 
+// Criar nova rota com parâmetros - Os parâmetros são inseridos após os dois pontos do olá
+app.get("/ola/:nome/:cargo", function(req, res){ // /Insira "localhost:8081/ola/marina/profe" em seu navegador
+ // res.send(req.params); // apresenta todos os parâmetros na mensagem
+    res.send("<h1> Olá " + req.params.nome + "</h1>"+"\n<h2> Seu cargo é: " + req.params.cargo + "</h2>");  // Adiciona HTML e apresenta os parâmetros nome e cargo
+    // Só pode chamar a função send uma vez!
+});
+
 // Abre servidor porta 8081 + função de callback /Deve ser colocada por último
 app.listen(8081, function(){
     console.log("Servidor executando na porta 8081!")
 });
+
+// Para reiniciar o servidor automaticamente, instalar o nodemon e ao executar utilizar nodemon ao invés de node
+// npm install nodemon -g
